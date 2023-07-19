27c1e9f1d69820ec6ac0e1f1e1403bfc4c7f21c3
Isabella Karibian <isabellakaribian@gmail.com>
2023-07-19 13:40:00 +0400

    stage0module4conditionsTask5

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -3,9 +3,9 @@ package school.mjc.stage0.conditions.task5;
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
 
-        if (year == 365){
+        if (year == 2004 || year ==2000){
             System.out.println("leap");
-        } else if (year==366) {
+        } else if (year==1900) {
             System.out.println("not leap");
         }
     }
