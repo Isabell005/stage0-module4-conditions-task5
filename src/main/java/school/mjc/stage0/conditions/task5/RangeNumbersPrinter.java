4862971e55b3d8e15c665ee79ab3c7fbb2f934c7
Isabella Karibian <isabellakaribian@gmail.com>
2023-07-19 13:29:49 +0400

    stage0module4conditionsTask5

--- src/main/java/school/mjc/stage0/conditions/task5/RangeNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/RangeNumbersPrinter.java
@@ -2,5 +2,26 @@ package school.mjc.stage0.conditions.task5;
 
 public class RangeNumbersPrinter {
     public void printRange(int number) {
+
+        String text;
+        switch (number) {
+            case (1),
+                    (2),
+                    (3),
+                    (4),
+                    (5):
+                text = "number is between 1 and 5";
+                break;
+            case (6),
+                    (7),
+                    (8),
+                    (9),
+                    (10):
+                text = "number is between 6 and 10";
+                break;
+            default:
+                text = "out of range";
+                break;
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -3,5 +3,30 @@ package school.mjc.stage0.conditions.task5;
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
 
+        String season;
+
+        switch (month) {
+            case (1),
+                (3),
+                (5),
+                (7),
+                (8),
+                (10),
+                (12):
+                season = "31";
+                break;
+            case (2):
+                season = "28";
+                break;
+            case (4), (6), (9), (11):
+                season = "30";
+                break;
+            default:
+                season = "wrong number!";
+                break;
+        }
+
     }
 }
+
+
